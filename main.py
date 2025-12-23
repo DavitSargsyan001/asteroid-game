732162198765ff38f4f7144ba97f3fda07f0d8ae
DavitSargsyan001 <sargsyan_davit@protonmail.com>
2025-12-22 21:12:17 -0800

    updates

--- main.py
+++ main.py
@@ -15,18 +15,27 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0
+    
 
+    
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    
 
-    while 1 == 1 :
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    
+    while True :
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for objects in drawable:
+            objects.draw(screen)
         pygame.display.flip()
         time_passed = clock.tick(60)
         dt = time_passed/1000
