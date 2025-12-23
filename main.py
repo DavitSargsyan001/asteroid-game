8a1744fb0a78e1e34e0a3346ec37284952faba10
DavitSargsyan001 <sargsyan_davit@protonmail.com>
2025-12-22 18:55:57 -0800

    drew the player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from logger import log_state
 import pygame
 from constants import *
+from player import *
 
 
 
@@ -15,17 +16,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while 1 == 1 :
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         time_passed = clock.tick(60)
         dt = time_passed/1000
         
-
    
 if __name__ == "__main__":
     main()
